7000ab77bb73572f68c11389e6c1f94036a5d616
Marc S <marcsalaver@gmail.com>
2020-03-16 14:36:28 -0700

    added changes to reflect time

--- script/script.js
+++ script/script.js
@@ -1,11 +1,22 @@
 $( document ).ready(function(){
-    function update() {
-        $('#today').html(moment().format('MMMM DD YYYY H:mm: ssA'));
-      }
-      
-      setInterval(update, 1000);
-  
-    
+  function update() {
+    $('#date').html(moment().format('MMMM DD YYYY'));   
+    $('#time').html(moment().format('h:mm:ss A'));   
+  }
+  //need to adjust for 12hr format
+  setInterval(update, 1000);
 
-      
+  $( "#scheduler" ).on("click", function() {
+    let btnAdd = $("<button>")
+  })
+
+  $( ".hour" ).on("click", function(){
+    let hour = $( ".hour" );
+
+    $('#myModal').modal(show)
+    //modal here for information input. 
+
+  })    
+
+
 })
